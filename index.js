bd3e0db5fc5be0a620ee6aeae692b931cc305b31
vivek15402 <vivek15402@gmail.com>
2024-06-23 06:21:54 +0530

    CORS

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 // Connect to MongoDB
 const mongoURL = process.env.MONGO_URL;
 mongoose.connect(mongoURL);
